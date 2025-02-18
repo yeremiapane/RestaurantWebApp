ee7b7e40a0c567a7aab609ed27fff2e8d0f12989
yerem <yeremiayosefan@mhs.unimed.ac.id>
2025-02-18 16:52:53 +0700

    Add CORS implementation

---
+++ Backend/middlewares/cors.go
@@ -0,0 +1,16 @@
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+func CORSMiddlewares() gin.HandlerFunc{
+	return func(c *gin.Context){
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+		c.Next()
+	}
+}
